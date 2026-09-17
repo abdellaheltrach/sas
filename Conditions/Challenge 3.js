717c9d708cf7f116e17ae273c426a2e48d5e48e1
abdellaheltrach <abdellah.eltrach@gmail.com>
2026-09-17 15:13:36 +0100

    fix: correct number parsing and add missing semicolons in Challenge 3

--- Conditions/Challenge 3.js
+++ Conditions/Challenge 3.js
@@ -1,9 +1,9 @@
 const PromptSync = require("prompt-sync")();
 
-let a = Number(PromptSync("entre le nombre A. "));
-let b = Number(PromptSync("entre le nombre B. "));
-let c = Number(PromptSync("entre le nombre C. "));
-let d = Number(PromptSync("entre le nombre D. "));
+let a = parseInt(PromptSync("entre le nombre A. "));
+let b = parseInt(PromptSync("entre le nombre B. "));
+let c = parseInt(PromptSync("entre le nombre C. "));
+let d = parseInt(PromptSync("entre le nombre D. "));
 
 
 
@@ -11,10 +11,10 @@ let sommeUn = a + b;
 let sommeDeux = c + d;
 
 if (sommeUn === sommeDeux) {
-    console.log(` la somme des deux valeurs entières sont identiques!  le triple de leur somme et ${sommeDeux * 3}`)
+    console.log(` la somme des deux valeurs entières sont identiques!  le triple de leur somme et ${sommeDeux * 3}`);
 
 } else {
-    console.log(` la somme des deux valeurs entières sont pas identiques!`)
+    console.log(` la somme des deux valeurs entières sont pas identiques!`);
 
 }
 
