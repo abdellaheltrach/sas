8b7ea4e2b7eec9b61c2026430cdae0d4ca2233e0
abdellaheltrach <abdellah.eltrach@gmail.com>
2026-09-17 14:38:50 +0100

    add chalange 1 in condition

---
+++ Conditions/Challenge 1.js
@@ -0,0 +1,10 @@
+const PromptSync = require("prompt-sync")();
+
+let a = Number(PromptSync("Entrez un nombre "));
+
+if (a % 2 === 0) {
+    console.log(`le numbers ${a} et pair.`);
+
+} else {
+    console.log(`le numbers ${a} et pair.`);
+}
