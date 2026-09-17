8f4058495ca23a502adb078fa47bccab0461ab9f
abdellaheltrach <abdellah.eltrach@gmail.com>
2026-09-17 17:29:35 +0100

    fix: correct variable name from 'année' to 'annes' in Challenge 5 for consistency

--- Conditions/Challenge 5.js
+++ Conditions/Challenge 5.js
@@ -1,6 +1,6 @@
 const PromptSync = require("prompt-sync")();
 
-let année = Number(PromptSync("entre comme bien d'année. "));
+let annes = Number(PromptSync("entre comme bien d'année. "));
 
 
 let choix = Number(PromptSync(
@@ -15,24 +15,24 @@ Quelles votre shoix? `));
 let result = null;
 switch (choix) {
     case 1:
-        result = année * 12;
+        result = annes * 12;
         console.log(`le nomber d'année ${annes} est egal ${result} mois.`)
 
         break;
     case 2:
-        result = année * 365;
+        result = annes * 365;
         console.log(`le nomber d'année ${annes} est egal ${result} jours.`)
         break;
     case 3:
-        result = année * 365 * 24;
+        result = annes * 365 * 24;
         console.log(`le nomber d'année ${annes} est egal ${result} Heures.`)
         break;
     case 4:
-        result = année * 365 * 24 * 60;
+        result = annes * 365 * 24 * 60;
         console.log(`le nomber d'année ${annes} est egal ${result} Minutes.`)
         break;
     case 5:
-        result = année * 365 * 24 * 60 * 60;
+        result = annes * 365 * 24 * 60 * 60;
         console.log(`le nomber d'année ${annes} est egal ${result} Secondes.`)
         break;
 
