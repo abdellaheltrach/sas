40cacfbeaf496608b25c0f3a6612519c7841cbcb
abdellaheltrach <abdellah.eltrach@gmail.com>
2026-09-22 10:58:44 +0100

    complete mini project

--- Mini-projet/Mini-project.js
+++ Mini-projet/Mini-project.js
@@ -1,8 +1,10 @@
 const PromptSync = require("prompt-sync")();
+const tableaux = [];
 
 do {
+    console.clear();
     let choix = 0;
-    let loop = true;
+    let repeat = true;
 
     console.log("[1] Ajouter un apprenant.")
     console.log("[2] Afficher tous les apprenants enregistres.")
@@ -10,38 +12,202 @@ do {
     console.log("[4] Mettre a jour la note d'un apprenant existant.")
     console.log("[5] Supprimer un apprenant par son prenom.")
     console.log("[6] Quitter le programme.")
-    choix = PromptSync("Quelles votre choix?. ")
+    choix = parseInt(PromptSync("Quelles votre choix?. "))
 
 
 
     switch (choix) {
         case 1:
-            console.log("[1] choix .")
-
+            choix1AjouterUnApparenant(tableaux);
             break;
         case 2:
-            console.log("[2] choix .")
+            choix2AfficherTousLesApprenants(tableaux);
 
             break;
         case 3:
-            console.log("[3] choix .")
+            choix3RechercherApparennantParPrenom(tableaux);
 
             break;
         case 4:
-            console.log("[4] choix .")
+            choix4MettreAJourNoteApprenant(tableaux);
 
             break;
         case 5:
-            console.log("[5] choix .")
+            choix5SupprimerApprenant(tableaux);
 
             break;
         case 6:
-            loop = false;
+            repeat = false;
             break;
         default:
+            console.log("fault choix!!")
 
             break;
     }
 
+    if (!repeat)
+        break;
 
-} while (loop)
+} while (true);
+
+
+function choix1AjouterUnApparenant(tableaux) {
+    console.clear()
+
+
+    let nom = PromptSync("enter le nom de apprenant: ")
+    let prenom = PromptSync("enter le prenom de apprenant: ")
+    let notes = PromptSync("enter les notes de apprenant: ")
+
+
+    const apprenant = {
+        nomDapparenant: nom,
+        prenomDappareant: prenom,
+        notesDapparent: notes
+
+    }
+    tableaux.push(apprenant)
+
+
+    PromptSync("continue?. ");
+    console.clear();
+}
+function choix2AfficherTousLesApprenants(tableaux) {
+    console.clear()
+
+    if (tableaux.length === 0)
+        console.log("Table Dapprenants est vide! ")
+
+    if (tableaux)
+        tableaux.forEach(apprenant => {
+
+            PrintApprenant(apprenant);
+
+        });
+
+
+
+    PromptSync("continue?. ");
+    console.clear();
+}
+
+function choix3RechercherApparennantParPrenom(tableaux) {
+    console.clear()
+    let prenomRechercher = PromptSync("enter le prenom de apprenant pour la recherch: ")
+    let found = false;
+
+    for (const apprenant of tableaux) {
+        if (apprenant.prenomDappareant === prenomRechercher) {
+            PrintApprenant(apprenant);
+            found = true;
+            break;
+        }
+    }
+
+    if (found === false)
+        console.log(`no apprenant avec le nome ${prenomRechercher} dans la list d'apprenant! `)
+
+    PromptSync("continue?. ");
+    console.clear();
+
+}
+function choix4MettreAJourNoteApprenant(tableaux) {
+    console.clear()
+    let prenomRechercher = PromptSync("enter le prenom de apprenant pour la recherch: ")
+    let found = false;
+
+    for (const apprenant of tableaux) {
+        if (apprenant.prenomDappareant === prenomRechercher) {
+            PrintApprenant(apprenant);
+            mettreAJourApprenantNotes(apprenant);
+
+            found = true;
+            break;
+        }
+    }
+
+    if (found === false) {
+        console.log(`no apprenant avec le nome ${prenomRechercher} dans la list d'apprenant! `)
+    }
+
+
+
+
+    PromptSync("continue?. ");
+    console.clear();
+
+}
+function choix5SupprimerApprenant(tableaux) {
+    console.clear()
+    let prenomRechercher = PromptSync("enter le prenom de apprenant pour la recherch: ")
+    let appSupprimer = false;
+
+    appSupprimer = SupprimerApprenant(tableaux, prenomRechercher);
+
+
+    if (appSupprimer) {
+        console.log("L'apprenant est supprimé !");
+    } else {
+        console.log("L'apprenant n'est pas supprimé !");
+    }
+
+
+    PromptSync("continue?. ");
+    console.clear();
+
+}
+
+
+//healpersss-----------------------------
+function PrintApprenant(apprenant) {
+    console.log("------------------------------------------");
+    console.log(`le nome d'apparenant: ${apprenant.nomDapparenant}.`);
+    console.log(`le prenom d'apparenant: ${apprenant.prenomDappareant}.`);
+    console.log(`le note d'apparenant: ${apprenant.notesDapparent}.`);
+    console.log("------------------------------------------");
+    console.log("");
+}
+
+function mettreAJourApprenantNotes(apprenant) {
+    let notes = PromptSync("enter les neveaux notes de apprenant: ")
+    apprenant.notesDapparent = notes;
+
+}
+
+function SupprimerApprenant(tableaux, prenomRechercher) {
+    let apparenantSupprimer = false;
+
+
+
+    for (let i = 0; i < tableaux.length; i++) {
+        if (tableaux[i].prenomDappareant === prenomRechercher) {
+            PrintApprenant(tableaux[i]);
+            const supprimerConfermation = PromptSync("supprimer cet apprenant [Y/N]? ").toLowerCase();
+
+            if (supprimerConfermation === "y") {
+
+                for (let j = i; j < tableaux.length; j++) {
+                    tableaux[j] = tableaux[j + 1];
+
+                    apparenantSupprimer = true;
+                    break;
+
+                }
+
+
+                tableaux.length -= 1;
+
+
+            } else {
+                apparenantSupprimer = false;
+            }
+
+        }
+    }
+
+
+
+    return apparenantSupprimer;
+}
+
+
